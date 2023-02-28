8ba491a1eabc67f8ed6ec2f0f37e070e3a64142e
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-02-28 12:30:38 +0530

    JSON imported in mongodb

--- foodish/src/App.js
+++ foodish/src/App.js
@@ -6,6 +6,9 @@ import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
+// foodishMERN
+// foodishMERN
+
 function App() {
   return (
     <Router>
